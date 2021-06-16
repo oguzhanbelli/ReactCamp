ca87296880ca0128e39c576725abeea64bbaf3a2
Oğuzhan Belli <75687376+oguzhanbelli@users.noreply.github.com>
2021-06-17 00:15:27 +0300

    170621 00:14 useSelector,dispatch

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,4 +1,5 @@
 import React,{useState} from 'react'
+import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router'
 
 import { Button, Container, Menu } from 'semantic-ui-react'
@@ -7,6 +8,8 @@ import SignedIn from './SignedIn'
 import SignedOut from './SignedOut'
 
 export default function Navi() {
+    const { cartItems } = useSelector(state => state.cart)
+    
 const [isAuthenticated, setIsAuthenticated] = useState(false)
 let history =useHistory()
 function handleSignOut(params) {
@@ -37,7 +40,11 @@ function handleSignIn(params) {
                 />
 
                 <Menu.Menu position='right' >
-                  <CartSummary/>
+                    {
+                        cartItems.length > 0 &&
+                         <CartSummary/>
+                    }
+         
 
 
                    {
